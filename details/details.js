d4894835bc80361773bf47f21e8dac9b2cfdf879
leonardo9245 <leonardo92reis@gmail.com>
2023-02-18 12:20:02 -0300

    first-commit

---
+++ details/details.js
@@ -0,0 +1,120 @@
+const container = document.querySelector('.container-detail');
+var ligthMode;
+//https://restcountries.com/v3.1/name/{name}?fullText=true
+
+const getLocalStorageInfo = () => {
+  let country;
+  let body = document.querySelector('body');
+  const info = JSON.parse(localStorage.getItem('country')) || {};
+
+  country = info.country;
+  ligthMode = info.light;
+
+  ligthMode
+    ? body.classList.add('light-mode')
+    : body.classList.remove('light-mode');
+
+  getCountry(country);
+};
+
+const saveLocalStoragePageInfo = value => {
+  localStorage.setItem(
+    'country',
+    JSON.stringify({ country: value, light: ligthMode })
+  );
+};
+
+const getCountry = async name => {
+  const resp = await fetch(`https://restcountries.com/v2/name/${name}`);
+  const data = await resp.json();
+
+  showInfoCountry(data[0]);
+};
+
+const getCountryByCode = async code => {
+  const resp = await fetch(`https://restcountries.com/v2/alpha/${code}`);
+  const data = await resp.json();
+
+  showInfoCountry(data);
+  saveLocalStoragePageInfo(data.name);
+};
+
+const showInfoCountry = list => {
+  console.log(list);
+  const getCurrencies = () => {
+    let currenciesListName = [];
+    let currenciesListSymbol = [];
+    for (let i of list.currencies) {
+      currenciesListSymbol.push(i.symbol);
+      currenciesListName.push(i.name);
+    }
+
+    let formatName = currenciesListName.splice(',').join(', ');
+    let formatSymbol = currenciesListSymbol.splice(',').join(', ');
+
+    let format = formatSymbol + ' ' + formatName;
+
+    return format;
+  };
+
+  const getLanguages = () => {
+    let languagesList = [];
+    for (let i of list.languages) {
+      languagesList.push(i.name);
+    }
+    return languagesList.splice(',').join(', ');
+  };
+
+  const formatNumber = number => {
+    return number.toLocaleString('en');
+  };
+
+  container.innerHTML = `
+  <div class="flag-details">
+    <img src="${list.flags.svg == '' ? list.flags.png : list.flags.svg}" alt="${
+    !'undefined ' ? list.flags.alt : ''
+  }"/>
+  </div>
+  <div class="box-detail-details">
+  <div class="country-detail-details">
+    <h3>${list.name.length > 20 ? list.nativeName : list.name}</h3>
+    <div class="details-p">
+    <p>Native Name: <span>${list.nativeName}</span></p>
+    <p>Population: <span>${formatNumber(list.population)}</span></p>
+    <p>Region: <span>${list.region}</span></p>
+    <p>Sub Region: <span>${list.subregion}</span></p>
+    <p>Capital: <span>${list.capital}</span></p>
+    <p>Top Level Domain: <span>${list.topLevelDomain[0]}</span></p>
+    <p>Currencies: <span>${getCurrencies()}</span></p>
+    <p>Languages: <span>${getLanguages()}</span></p>
+    </div>
+    </div>
+    <div class="border-details">
+    <p >Border Countries: </p>
+    </div>
+  </div>
+  `;
+
+  let countriesList = [];
+  let buttonDiv = document.querySelector('.border-details');
+
+  if (list.borders != undefined) {
+    for (let i of list.borders) {
+      countriesList.push(i);
+    }
+
+    if (countriesList.length > 4) {
+      countriesList.length = 4;
+    }
+    for (let i of countriesList) {
+      let button = document.createElement('button');
+      button.innerHTML = i;
+      buttonDiv.appendChild(button);
+
+      button.addEventListener('click', () => {
+        getCountryByCode(i);
+      });
+    }
+  }
+};
+getLocalStorageInfo();
---
+++ script.js
@@ -0,0 +1,150 @@
+//index page variables
+const searchInput = document.getElementById('search-countries');
+const countriesEl = document.getElementById('countries');
+const countriesBox = document.querySelector('.country-box');
+const filterBtn = document.getElementById('filter-countries');
+const countriesList = [];
+var bodyTypeLight = false;
+//functions index page
+
+window.onload = function () {
+  filterBtn.selectIndex = 0;
+  searchInput.value = '';
+  const info = JSON.parse(localStorage.getItem('country')) || {};
+  if (info.light) {
+    lightMode();
+  }
+};
+
+//onclick="location.href='/details/details.html';
+//onclick="location.href='/details/details.html';"
+const createCountryElement = () => {
+  const formatNumber = number => {
+    return number.toLocaleString('en');
+  };
+
+  countriesList[0].forEach(val => {
+    countriesEl.innerHTML += `<div id="${val.name}" class="country-box ${
+      val.region
+    } " onclick="location.href='/details/details.html';"><img src="${
+      val.flags.svg
+    }" alt="${val.flags.alt}"/><div class="country-details"><h3>${
+      val.name
+    }</h3><p>Population: <span>${formatNumber(
+      val.population
+    )}</span></p> <p>Region: <span>${val.region}</span></p> <p>Capital: <span>${
+      val.capital
+    }</span></p></div></div>`;
+    //console.log(val);
+  });
+};
+
+const filterOptions = () => {
+  let options = [];
+  countriesList[0].forEach(value => {
+    options.push(value.region);
+  });
+
+  let newOptions = options.filter((i, n) => options.indexOf(i) === n);
+
+  newOptions.forEach(val => {
+    filterBtn.innerHTML += `<option value="${val}">${val}</option>`;
+  });
+};
+
+const getAllcountries = async () => {
+  const resp = await fetch('https://restcountries.com/v2/all');
+  const data = await resp.json();
+
+  //console.log(data[0]);
+  countriesList.push(data);
+  createCountryElement();
+  filterOptions();
+};
+
+const lightMode = () => {
+  const body = document.querySelector('body');
+  const button = document.getElementById('page-mode');
+  const elements = document.querySelectorAll(
+    'header, input, select, country-box'
+  );
+
+  body.classList.toggle('light-mode');
+  button.classList.toggle('light-mode');
+
+  button.classList.contains('light-mode')
+    ? (button.innerHTML = '<i class="fa-solid fa-moon"></i>Dark Mode')
+    : (button.innerHTML = '<i class="fa-solid fa-sun"></i>Light Mode');
+
+  for (let i of elements) {
+    i.classList.toggle('light-mode');
+  }
+
+  bodyTypeLight = !bodyTypeLight;
+};
+
+const FilterCountries = value => {
+  let countries = document.querySelectorAll('.country-box');
+
+  switch (value) {
+    case 'all':
+      countries.forEach(countries => (countries.style.display = 'block'));
+      break;
+    case value:
+      countries.forEach(val => {
+        val.classList.contains(value)
+          ? (val.style.display = 'block')
+          : (val.style.display = 'none');
+      });
+
+      break;
+    default:
+      break;
+  }
+};
+
+const getSearchCountry = search => {
+  let countries = document.querySelectorAll('.country-box');
+  let searchLowerCase = search.toLowerCase();
+
+  countries.forEach(val => {
+    let countryName = val.querySelector('h3').innerText.toLowerCase();
+
+    val.style.display = 'block';
+
+    if (!countryName.includes(searchLowerCase)) {
+      val.style.display = 'none';
+    }
+  });
+};
+
+const saveLocalStoragePageInfo = (value, bodyMode) => {
+  localStorage.setItem(
+    'country',
+    JSON.stringify({ country: value, light: bodyMode })
+  );
+};
+
+//Events index page
+
+searchInput.addEventListener('input', e => {
+  const search = e.target.value;
+
+  getSearchCountry(search);
+});
+
+document.addEventListener('click', e => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest('div');
+  if (parentEl && parentEl.classList.contains('country-box')) {
+    saveLocalStoragePageInfo(parentEl.id, bodyTypeLight);
+  }
+});
+
+filterBtn.addEventListener('change', e => {
+  const filterValue = e.target.value;
+
+  FilterCountries(filterValue);
+});
+
+getAllcountries();
